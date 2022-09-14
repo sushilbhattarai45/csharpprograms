dec4e13bf66d70a0ad1977a7e6705998ae6a1e8e
sushilbhattarai45 <bhattaraisushil005@gmail.com>
2022-09-14 19:27:47 +0545

    ok

--- middle.cs
+++ middle.cs
@@ -12,23 +12,24 @@ class HelloWorld
 
         Console.WriteLine("enter first  third  number");
         int c = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("greatest num is number");
 
-        if (a > b && a > c)
+        Console.WriteLine("Middle num is number");
+
+        if (a > b && a < c || a < b && a > c)
         {
             Console.WriteLine(a);
         }
-        else if (b > a && b > c)
+        else if (b > a && b > c || b < a && b < c)
         {
             Console.WriteLine(b);
         }
-        else if (c > a && c > b)
+        else if (c > a && c > b || c < a && c < b)
         {
             Console.WriteLine(c);
         }
         else
         {
-            Console.WriteLine("2 numbers are equals error");
+            Console.WriteLine("Error: Some numbers are equal");
         }
 
     }
