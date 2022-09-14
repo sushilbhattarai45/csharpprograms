566f268eb94a367ef0d58e5c8e8ebc112b8b3f91
sushilbhattarai45 <bhattaraisushil005@gmail.com>
2022-09-14 19:40:32 +0545

    ok

---
+++ sumofodd.cs
@@ -0,0 +1,22 @@
+
+using System;
+class HelloWorld
+{
+    static void Main()
+    {
+        Console.WriteLine("enter  number");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int sum = 0;
+        while (n != 0)
+        {
+            int rem = n % 10;
+            if (rem % 2 != 0)
+            {
+                sum = sum + rem;
+            }
+            n = n / 10;
+        }
+        Console.WriteLine(sum);
+
+    }
+}
