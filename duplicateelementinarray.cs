b67fb196741d3a0aa1a21d4b3b4e815ff4f84963
sushilbhattarai45 <bhattaraisushil005@gmail.com>
2023-01-07 09:07:13 +0545

    string pattern

--- duplicateelementinarray.cs
+++ duplicateelementinarray.cs
@@ -1,33 +1,33 @@
-
+//sushil's code
 using System;
 
 public class HelloWorld
 {
-  public static void Main (String[]args)
-  {
-    int count = 0;
-    int [] arr ={1,2,3,4,1,2,3};
-    for (int i = 0; i <arr.Length; i++)
-      {
-	for (int j = arr.Length-1; j>i; j--)
-	  {
+    public static void Main(String[] args)
+    {
+        int count = 0;
+        int[] arr = { 1, 2, 3, 4, 1, 2, 3 };
+        for (int i = 0; i < arr.Length; i++)
+        {
+            for (int j = arr.Length - 1; j > i; j--)
+            {
 
-	      if(i!=j)
-	      {
-	          if(arr[i]==arr[j])
-	          {	        
-	              count=count+1;
-	              
-	          }
-	      }
-	      
-	   
-	  }
+                if (i != j)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        count = count + 1;
+
+                    }
+                }
 
 
+            }
 
 
-      }
-      Console.WriteLine(count);
-  }
+
+
+        }
+        Console.WriteLine(count);
+    }
 }
--- programs/programs.cs
+++ programs/programs.cs
@@ -1,17 +1,88 @@
 using System;
-namespace chorko
+using System.Windows.Forms;
+
+namespace Calculator
 {
-    class HelloWorld
+    public partial class Form1 : Form
     {
-        static void Main(String[] args)
+        double resultValue = 0;
+        string operationPerformed = "";
+        bool isOperationPerformed = false;
+
+        public Form1()
         {
-            Console.WriteLine("Let me decide who is pen chor");
+            InitializeComponent();
+        }
 
-            for (int i = 0; i < 5; i++)
+        private void button_click(object sender, EventArgs e)
+        {
+            if ((textBox_Result.Text == "0") || (isOperationPerformed))
+                textBox_Result.Clear();
+
+            isOperationPerformed = false;
+            Button button = (Button)sender;
+            if (button.Text == ".")
             {
-                Console.WriteLine("rAJESH IS  is pen chor");
-
+                if (!textBox_Result.Text.Contains("."))
+                    textBox_Result.Text = textBox_Result.Text + button.Text;
             }
+            else
+                textBox_Result.Text = textBox_Result.Text + button.Text;
+        }
+
+        private void operator_click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            if (resultValue != 0)
+            {
+                button22.PerformClick();
+                operationPerformed = button.Text;
+                labelCurrentOperation.Text = resultValue + " " + operationPerformed;
+                isOperationPerformed = true;
+            }
+            else
+            {
+                operationPerformed = button.Text;
+                resultValue = double.Parse(textBox_Result.Text);
+                labelCurrentOperation.Text = resultValue + " " + operationPerformed;
+                isOperationPerformed = true;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox_Result.Text = "0";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox_Result.Text = "0";
+            resultValue = 0;
+        }
+
+        private void button22_Click(object sender, EventArgs e)
+        {
+            switch (operationPerformed)
+            {
+                case "+":
+                    textBox_Result.Text = (resultValue + double.Parse(textBox_Result.Text)).ToString();
+                    break;
+                case "-":
+                    textBox_Result.Text = (resultValue - double.Parse(textBox_Result.Text)).ToString();
+                    break;
+                case "*":
+                    textBox_Result.Text = (resultValue * double.Parse(textBox_Result.Text)).ToString();
+                    break;
+                case "/":
+                    textBox_Result.Text = (resultValue / double.Parse(textBox_Result.Text)).ToString();
+                    break;
+                default:
+                    break;
+            }
+
+            resultValue = double.Parse(textBox_Result.Text);
+            labelCurrentOperation.Text = "";
         }
     }
 }
